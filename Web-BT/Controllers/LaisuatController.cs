c7c908ab3788b910bb63d50b95ce7b6502c38d95
May <ADMIN@MAY>
2023-05-19 22:11:51 +0700

    Add project files.

---
+++ Web-BT/Controllers/LaisuatController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_BT.Controllers
+{
+    public class LaisuatController : Controller
+    {
+        public IActionResult Giaodien()
+        {
+            return View();
+        }
+    [HttpPost]
+        public IActionResult Giaodien(int sotiengui, int laisuatgui, int kyhangui)
+        {
+            try
+            {
+                if(laisuatgui == 0)
+                {
+                    ViewData["Message1"] = "Lãi suất gửi phải lớn hơn";
+                }   
+                if (kyhangui == 0)
+                {
+                    ViewData["Message2"] = "Kỳ hạn gửi phải lớn hơn";
+                }
+                if (sotiengui == 0)
+                {
+                    ViewData["Message3"] = "Số tiền gửi phải lớn hơn 0";
+                }
+                ViewData["Sotiengui"] = sotiengui;
+                ViewData["Laisuatgui"] = laisuatgui;
+                ViewData["Kyhangui"] = kyhangui;
+         
+                ViewData["Sotienlainhanduoc"] = (sotiengui * laisuatgui / 100) * ((double)kyhangui / 12);
+                ViewData["Tongsotiennhanduockhidenhan"] = ((sotiengui * laisuatgui / 100) * ((double)kyhangui / 12) + sotiengui);
+            }
+            catch (DivideByZeroException)
+            {
+                ViewData["Message"] = "Thong tin nhap vao khong phu hop";
+
+            }
+            return View();
+        }
+    }
+}
---
+++ Web-BT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_BT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
